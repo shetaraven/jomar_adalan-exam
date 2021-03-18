359871cd433d0b16612ff28018fdbca29315920f
Jomar Yaco Adalan <jya@Jomars-MacBook-Pro.local>
2021-03-18 17:54:07 +0800

    sql

---
+++ sociablekit_2021-03-18.sql
@@ -0,0 +1,62 @@
+﻿# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 5.7.30)
+# Database: sociablekit
+# Generation Time: 2021-03-18 09:52:59 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table youtube_channel_videos
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `youtube_channel_videos`;
+
+CREATE TABLE `youtube_channel_videos` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `channel_id` int(11) NOT NULL,
+  `video_link` varchar(1000) NOT NULL DEFAULT '',
+  `title` varchar(300) NOT NULL DEFAULT '',
+  `description` text NOT NULL,
+  `thumbnail` varchar(1000) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+# Dump of table youtube_channels
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `youtube_channels`;
+
+CREATE TABLE `youtube_channels` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `profile_pic` varchar(1000) NOT NULL DEFAULT '',
+  `name` varchar(300) NOT NULL DEFAULT '',
+  `description` varchar(1000) NOT NULL DEFAULT '',
+  `banner_url` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
